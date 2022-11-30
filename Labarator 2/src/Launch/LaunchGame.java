4e1c15ff7a05025dfe3cb531c31b9dc1874e93d3
Invop <shandrykjj512@gmail.com>
2022-11-30 19:38:16 +0200

    Hi

--- Labarator 2/src/Launch/LaunchGame.java
+++ Labarator 2/src/Launch/LaunchGame.java
@@ -1,9 +1,10 @@
 package Launch;
+import acm.graphics.GLabel;
 import acm.program.*;
 public class LaunchGame extends GraphicsProgram {
 	public void run()
 	{
 			this.setSize(500,600);
-	
+			GLabel label = new GLabel("HI!");
 	}
 }
