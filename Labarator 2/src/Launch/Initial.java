b7dd02c9932d0305f70c3f13a83a73d9a85abf6d
Invop <shandrykjj512@gmail.com>
2022-12-02 20:43:49 +0200

    TenRowsOfBricks(),createBrick() methods added to Initial class

--- Labarator 2/src/Launch/Initial.java
+++ Labarator 2/src/Launch/Initial.java
@@ -1,47 +1,94 @@
 package Launch;
 
+import java.awt.Color;
+
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class Initial extends GraphicsProgram{
 
 /** Width and height of application window in pixels */
-	public static final int APPLICATION_WIDTH = 1000;
-	public static final int APPLICATION_HEIGHT = 1000;
+	public static final int APPLICATION_WIDTH = 500;
+	public static final int APPLICATION_HEIGHT = 700;
 
 /** Dimensions of game board (usually the same) */
-	private static final int WIDTH = APPLICATION_WIDTH;
-	private static final int HEIGHT = APPLICATION_HEIGHT;
+	public static final int WIDTH = APPLICATION_WIDTH;
+	public static final int HEIGHT = APPLICATION_HEIGHT;
 
 /** Dimensions of the paddle */
-	private static final int PADDLE_WIDTH = 60;
-	private static final int PADDLE_HEIGHT = 10;
+	public static final int PADDLE_WIDTH = 60;
+	public static final int PADDLE_HEIGHT = 10;
 
 /** Offset of the paddle up from the bottom */
-	private static final int PADDLE_Y_OFFSET = 30;
+	public static final int PADDLE_Y_OFFSET = 30;
 
 /** Number of bricks per row */
-	private static final int NBRICKS_PER_ROW = 10;
+	public static final int NBRICKS_PER_ROW = 10;
 
 /** Number of rows of bricks */
-	private static final int NBRICK_ROWS = 10;
+	public static final int NBRICK_ROWS = 10;
 
 /** Separation between bricks */
-	private static final int BRICK_SEP = 4;
+	public static final int BRICK_SEP = 4;
 
 /** Width of a brick */
-	private static final int BRICK_WIDTH =
+	public static final int BRICK_WIDTH =
 	  (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
 
 /** Height of a brick */
-	private static final int BRICK_HEIGHT = 8;
+	public static final int BRICK_HEIGHT = 8;
 
 /** Radius of the ball in pixels */
-	private static final int BALL_RADIUS = 10;
+	public static final int BALL_RADIUS = 10;
 
 /** Offset of the top brick row from the top */
-	private static final int BRICK_Y_OFFSET = 70;
+	public static final int BRICK_Y_OFFSET = 70;
 
 /** Number of turns */
-	private static final int NTURNS = 3;
-	
+	public static final int NTURNS = 3;
+		/**
+		 * @author Andrii
+		 * @param yCoordinateRow
+		 * @param xCoordinate
+		 */
+	   public void tenRowsOfBricks() {    
+	        double yCoordinateRow = BRICK_Y_OFFSET;
+	        double xCoordinate;
+	        for (int i = 0; i < NBRICK_ROWS; i++) {
+	        	xCoordinate =0; 
+	            for (int j = 0; j < NBRICKS_PER_ROW; j++) {    
+	               createBrick(xCoordinate, yCoordinateRow, i);
+	                xCoordinate = xCoordinate + BRICK_WIDTH + BRICK_SEP;
+	            }
+	            yCoordinateRow = yCoordinateRow + BRICK_HEIGHT + BRICK_SEP;
+	        }
+	    }
+	   /**
+		 * @author Andrii
+		 * @param xCoordinate
+		 * @param yCoordinateRow
+		 * @param NumLine
+		 * @
+		 */
+	   private void createBrick(double xCoordinate, double yCoordinateRow, int NumLine) {
+	        // create a brick
+	        GRect brick = new GRect(xCoordinate, yCoordinateRow, BRICK_WIDTH, BRICK_HEIGHT);
+	        brick.setFilled(true);
+	        if (NumLine == 0 || NumLine == 1) {
+	        	brick.setColor(Color.RED);
+	        }
+	        if (NumLine == 2 || NumLine == 3) {
+	        	brick.setColor(Color.ORANGE);
+	        }
+	        if (NumLine == 4 || NumLine == 5) {
+	        	brick.setColor(Color.YELLOW);
+	        }
+	        if (NumLine== 6 || NumLine == 7) {
+	        	brick.setColor(Color.GREEN);
+	        }
+	        if (NumLine == 8 || NumLine == 9) {
+	        	brick.setColor(Color.CYAN);
+	        }
+	        add(brick);
+	    }
 }
--- Labarator 2/src/Launch/LaunchGame.java
+++ Labarator 2/src/Launch/LaunchGame.java
@@ -1,18 +1,10 @@
 package Launch;
-import acm.graphics.*;
-import acm.program.*;
-import acm.util.*;
-
-import java.applet.*;
-import java.awt.*;
-import java.awt.event.*;
-
 public class LaunchGame extends Initial {
 /* Method: run() */
 /** Runs the Breakout program. */
 	public void run() {
 		/* You fill this in, along with any subsidiary methods */
 		this.setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
+		tenRowsOfBricks();
 	}
-
 }
